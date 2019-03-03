1f3c3627489c4cbab11ad74e2e31fa94721ccf71
Pantelis <tampantelis@gmail.com>
2019-03-03 19:11:39 +0200

    added react-router

--- src/app.js
+++ src/app.js
@@ -1,7 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
+const ExpenseDashboardPage = () => (
+  <div>this is dahborad component</div>
+);
 
-ReactDOM.render(<p>this is a boiler plate!</p>, document.getElementById('app'));
+const AddExpensePage = () => (
+  <div>this is add expense component</div>
+);
+
+const EditExpensePage = () => (
+  <div>this is edit expense component</div>
+);
+
+const HelpPage = () => (
+  <div>this is help component</div>
+);
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ExpenseDashboardPage} exact={true} />
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/edit" component={EditExpensePage} />
+      <Route path="/help" component={HelpPage} />
+
+    </div>
+  </BrowserRouter>
+);
+
+
+ReactDOM.render(routes, document.getElementById('app'));
