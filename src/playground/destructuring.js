f73c587c873802dcb0a7864497fae606636ce557
Pantelis <tampantelis@gmail.com>
2019-03-07 20:31:45 +0200

    Array destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -17,14 +17,27 @@
 //   console.log(`Its ${temp}C in ${city}`);
 // }
 
-const book = {
-  title: 'Ego is the Enemy',
-  authos: 'Ryan Holiday',
-  publisher: {
-    name: 'Penguin'
-  }
-};
+// const book = {
+//   title: 'Ego is the Enemy',
+//   authos: 'Ryan Holiday',
+//   publisher: {
+//     name: 'Penguin'
+//   }
+// };
 
-const {name: publisherName = 'Selg-Published'} = book.publisher;
+// const {name: publisherName = 'Selg-Published'} = book.publisher;
 
-console.log (publisherName);
+// console.log (publisherName);
+
+// const address = ['1299 s juniper street','Philadelphia','Pennsylvania','19147'];
+
+// const [,city , state = 'New York'] = address;
+
+// console.log(`You are in ${city} ${state}`);
+
+const item = ['Coffee (hot)','$2.00','$2.50','$2.75'];
+
+const [coffee,, medium = 'New York'] = item;
+
+
+console.log(`A medium ${coffee} costs ${medium}`);
