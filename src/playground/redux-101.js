0cc7a1a1342f46a66421bbf316f24765a04be3de
Pantelis <tampantelis@gmail.com>
2019-03-09 19:34:00 +0200

    action generators

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,20 +1,52 @@
 import {createStore} from 'redux';
 
+
+
+const incrementCount = ({ incrementBy = 1 }= {}) => {
+  return {
+    type: 'INCREMENT',
+    incrementBy
+  }
+};
+
+const decrementCount =({ decrementBy = 1}= {}) => {
+  return {
+    type: 'DECREMENT',
+    decrementBy
+  };
+};
+
+const resetCount =() => {
+  return {
+    type: 'RESET'
+  };
+};
+
+const setCount =({ setBy = 1}= {}) => {
+  return {
+    type: 'SET',
+    setBy
+  };
+};
+
+
 const store = createStore((state = { count: 0 }, action) => {
 switch (action.type) {
   case 'INCREMENT':
-  const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
     return {
-      count: state.count + incrementBy
+      count: state.count + action.incrementBy
     };
   case 'DECREMENT':
-  const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
     return {
-      count: state.count - decrementBy
+      count: state.count - action.decrementBy
     }
     case 'RESET':
     return {
       count: 0
+    }
+    case 'SET':
+    return {
+      count: action.setBy
     }
   default:
   return state;
@@ -27,30 +59,18 @@ const unsubscribe = store.subscribe(() => {
   console.log(store.getState());
 });
 
+store.dispatch(incrementCount({incrementBy:5 }));
+
+store.dispatch(incrementCount());
 
 
-store.dispatch(
-{
-  type: 'INCREMENT',
-  incrementBy: 5
-});
 
-store.dispatch(
-  {
-    type: 'INCREMENT'
-  });
-  
+store.dispatch(resetCount());
 
-store.dispatch(
-  {
-    type: 'RESET'
-  });
+store.dispatch(decrementCount());
+    
+store.dispatch(decrementCount({ decrementBy: 10 }));
 
-
-store.dispatch(
-  {
-    type: 'DECREMENT',
-    decrementBy: 10
-  });
+store.dispatch(setCount({ setBy: 100}));
 
 
