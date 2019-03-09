8c86d32667b9b2144eaccd71047b39a3a1423269
Pantelis <tampantelis@gmail.com>
2019-03-09 20:07:37 +0200

    expenser demo reducers

--- src/playground/redux-expenser.js
+++ src/playground/redux-expenser.js
@@ -1,5 +1,39 @@
 import { createStore, combineReducers } from 'redux';
 
+const expensesReducerDefaultState =[];
+
+
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+
+};
+
+const filterReducerDefaultState = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+};
+const filterReducer = (state = filterReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+
+};
+
+const store = createStore(
+  combineReducers({
+    expenses: expensesReducer,
+    filter: filterReducer
+  }));
+
+console.log(store.getState());
+
+
 const demoState = {
   expenses: [{
     id: 'asdas',
