c57e2893d6f0a7b53f91a3c4dacb9a0cc26ee812
Pantelis <tampantelis@gmail.com>
2019-03-11 19:26:04 +0200

    sorting redux data

--- src/playground/redux-expenser.js
+++ src/playground/redux-expenser.js
@@ -126,7 +126,13 @@ const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate}) => {
 
 
     return startDateMatch && endDateMatch && textMatch;
-  });
+  }).sort((a , b) => {
+    if (sortBy === 'date') {
+      return a.createdAt < b.createdAt ? 1 : -1;
+    } else if (sortBy === 'amount') {
+      return a.amount < b.amount ? 1 : -1;
+    } 
+  });;
 };
 
 
@@ -142,18 +148,18 @@ store.subscribe (() => {
   console.log(visibleExpenses);
 });
 
-const expenseOne = store.dispatch (addExpense({ description: 'Rent', amount: 100, createdAt: 1000}));
-const expenseTwo = store.dispatch (addExpense({ description: 'Coffee', amount: 300, createdAt: -2000}));
+const expenseOne = store.dispatch (addExpense({ description: 'Rent', amount: 100, createdAt: -11000}));
+const expenseTwo = store.dispatch (addExpense({ description: 'Coffee', amount: 300, createdAt: 2000}));
 
 // store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 
 // store.dispatch(editExpense(expenseTwo.expense.id,{ amount: 500}));
 
-store.dispatch(setTextFilter('Rent'));
+// store.dispatch(setTextFilter('Rent'));
 
 // store.dispatch(setTextFilter());
 
-// store.dispatch(sortByAmount());
+store.dispatch(sortByAmount());
 // store.dispatch(sortByDate());
 
 // store.dispatch(setStartDate(125));
