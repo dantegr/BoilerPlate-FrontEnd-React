fbc35a67b09e6ea192c399513b27ac4a0f7b31ec
Pantelis <tampantelis@gmail.com>
2019-03-24 13:12:06 +0200

    more components testing snapshot dynamicly

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -6,9 +6,4 @@ test('should render Header correctly', () => {
   const wrapper = shallow(<Header />);
   expect(wrapper).toMatchSnapshot();
 
-  // expect(wrapper.find('h1').length).toBe(1);
-  // const renderer = new ReactShallowRenderer();
-  // renderer.render(<Header/>);
-  // expect(renderer.getRenderOutput()).toMatchSnapshot();
-  // console.log(renderer.getRenderOutput());
 });
