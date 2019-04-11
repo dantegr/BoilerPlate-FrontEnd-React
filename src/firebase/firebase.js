255eb614908ae6066206edb800dc0462b2bac715
Pantelis <tampantelis@gmail.com>
2019-04-11 21:06:52 +0300

    setted up test database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: "AIzaSyBbia0ZfYSfgnYxx5tWLmW0mZd5r7TtQmI",
-  authDomain: "expenser-a88d3.firebaseapp.com",
-  databaseURL: "https://expenser-a88d3.firebaseio.com",
-  projectId: "expenser-a88d3",
-  storageBucket: "expenser-a88d3.appspot.com",
-  messagingSenderId: "158575660410"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
