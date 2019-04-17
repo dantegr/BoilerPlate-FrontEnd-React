76991b2be17fc8be6954e286e15fbacf6495b0e1
Pantelis <tampantelis@gmail.com>
2019-04-17 19:54:40 +0300

    css styled header component

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -7,7 +7,7 @@ export const LoginPage = ({ startLogin }) => (
     <div className="box-layout__box">
       <h1 className="box-layout__title">Expenser</h1>
       <p>Manage your expenses the right way!</p>
-      <button onClick={startLogin}>Login</button>
+      <button className="button" onClick={startLogin}>Login with Google!</button>
     </div>
     
   </div>
