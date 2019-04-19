e43d998178555663acb7cb68cd023ca335c392e0
Pantelis <tampantelis@gmail.com>
2019-04-19 21:50:03 +0300

    css styled edit expense component

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -55,9 +55,9 @@ export default class ExpenseForm extends React.Component {
   };
   render() {
     return (
-      <div>
-        {this.state.error && <p>{this.state.error}</p>}
-        <form onSubmit={this.onSubmit}>
+       
+        <form className="form" onSubmit={this.onSubmit}>
+          {this.state.error && <p className="form__error">{this.state.error}</p>}
           <input
             className="text-input"
             type="text"
@@ -89,9 +89,11 @@ export default class ExpenseForm extends React.Component {
             onChange={this.onNoteChange}
           >
           </textarea>
-          <button>Add Expense</button>
+          <div>
+            <button className="button">Save Expense</button>
+          </div>
         </form>
-      </div>
+      
     )
   }
 }
